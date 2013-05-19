421b2508bd8a972a4fe9e6c96b18702774350ba5
Ronan <ronandoyle91@gmail.com>
2013-05-19 13:09:58 +0100

    Added in app launcher icon

--- Android/gen/com/droidgo/R.java
+++ Android/gen/com/droidgo/R.java
@@ -13,10 +13,11 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int button_shape=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int play=0x7f020004;
-        public static final int stop=0x7f020005;
+        public static final int droid_go_icon=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int play=0x7f020005;
+        public static final int stop=0x7f020006;
     }
     public static final class id {
         public static final int cameraView=0x7f060006;
